a619967e2db491e06def9d8b01b3aeaf80194cac
George Wong <george@somewhereville.com>
2013-07-28 20:56:53 -0400

    tests.js file

---
+++ tests.js
@@ -0,0 +1,17 @@
+/*
+ * tests.js
+ * 
+ * A group of simple JavaScript test
+ * scripts that allow you to check if
+ * you are properly importing this file
+ * from its remotely-hosted (web)
+ * location.
+ * 
+ * A: George Wong
+ * C: 28.7.2013
+ * E: 28.7.2013
+ */
+
+function writeHello() {
+  document.write ("<h1>HELLO</h1>");
+}
