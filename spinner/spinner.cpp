d1006b99d78cab85bbba24a5e23d9696a8774b64
George Wong <georgewong93@gmail.com>
2013-08-12 00:49:01 -0400

    Framework for combined spin program

---
+++ spinner/spinner.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+
+
+
+void process (std::vector<time_t> delay, std::vector<int_fast8_t> amount);
+void thread (std::vector<time_t> delay, std::vector<int_fast8_t> amount);
+void mutex_lock (time_t delay, int_fast8_t frequency, int_fast8_t depth);
+void cpu_computation (time_t delay, int_fast8_t amount);
+void cpu_cache (time_t delay, int_fast8_t amount);
+void mem_eat (time_t delay, int_fast8_t amount);
+void mem_fail (time_t delay, int_fast8_t frequency);
+void disk_io (time_t delay, int_fast8_t amount);
+void net_udp (time_t delay, int_fast8_t amount);
+void net_tcp (time_t delay, int_fast8_t amount);
+void usage ();
+
+
+int main (int argc, char **argv) {
+
+
+
+  
+
+
+
+
+
+
+
+  return 0;
+}
+
+void process (std::vector<time_t> delay, std::vector<int_fast8_t> amount) {
+
+}
+
+
+void thread (std::vector<time_t> delay, std::vector<int_fast8_t> amount) {
+
+
+}
+
+void mutex_lock (time_t delay, int_fast8_t frequency, int_fast8_t depth) {
+
+
+}
+
+
+void cpu_computation (time_t delay, int_fast8_t amount) {
+
+
+  // Run Collatz
+
+
+}
+
+void cpu_cache (time_t delay, int_fast8_t amount) {
+
+
+}
+
+void mem_eat (time_t delay, int_fast8_t amount) {
+
+
+
+}
+
+void mem_fail (time_t delay, int_fast8_t frequency) {
+
+
+
+}
+
+void disk_io (time_t delay, int_fast8_t amount) {
+
+
+}
+
+void net_udp (time_t delay, int_fast8_t amount) {
+
+
+}
+
+void net_tcp (time_t delay, int_fast8_t amount) {
+
+
+
+}
+
+
+
+void usage () {
+
+
+}
+
+
+
+
+
+
+
